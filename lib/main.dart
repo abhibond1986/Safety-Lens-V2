e30e45932b6c4708f590584331747c5173a5ad16
abhibond1986 <abhi.nitks1986@gmail.com>
2026-06-04 23:13:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'services/local_db.dart';
 import 'services/sync_service.dart';
 import 'screens/splash_screen.dart';
